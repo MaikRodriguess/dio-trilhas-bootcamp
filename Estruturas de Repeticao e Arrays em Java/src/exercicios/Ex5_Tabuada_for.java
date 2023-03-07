1107c36a1ec7bce786d1d4e8f22106b5ab123fa2
Maik Rodrigues <69226200+MaikRodriguess@users.noreply.github.com>
2023-03-07 12:14:08 -0300

    finalizado

---
+++ Estruturas de Repeticao e Arrays em Java/src/exercicios/Ex5_Tabuada_for.java
@@ -0,0 +1,16 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class Ex5_Tabuada_for {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Tabuada: ");
+        int tabuada = scan.nextInt();
+
+        for (int i = 1; i <= 10 ; i++ ){
+            System.out.println(tabuada + " X " + i + " = " + (tabuada * i));
+        }
+    }
+}
