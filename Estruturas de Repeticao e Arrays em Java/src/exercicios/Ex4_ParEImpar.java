5805b73e97ad7a0da3e861403fc66ad4650fe92d
Maik Rodrigues <69226200+MaikRodriguess@users.noreply.github.com>
2023-03-07 00:14:28 -0300

    Exercicio finalizado

---
+++ Estruturas de Repeticao e Arrays em Java/src/exercicios/Ex4_ParEImpar.java
@@ -0,0 +1,32 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class Ex4_ParEImpar {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int quantNumeros;
+        int numero;
+
+        int quantNumerosPar = 0, quantNumeroImpar = 0;
+
+        System.out.println("Quantidade de números: ");
+        quantNumeros = scan.nextInt();
+
+
+        int contador = 0;
+        do{
+            System.out.println("Número: ");
+            numero = scan.nextInt();
+
+            if (numero % 2 == 0) quantNumerosPar++;
+            else quantNumeroImpar++;
+
+            contador++;
+        }while (contador < quantNumeros);
+        System.out.println("Quantidade de números pares: " + quantNumerosPar);
+        System.out.println("Quantidade de números impares: " + quantNumeroImpar);
+    }
+}
