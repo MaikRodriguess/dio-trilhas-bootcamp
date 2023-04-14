f4bcffad6b1c45a2dba8363769e0fe62515d6c34
Maik Rodrigues <69226200+MaikRodriguess@users.noreply.github.com>
2023-04-14 00:05:23 -0300

    A terminar..

---
+++ Spring Boot/dio-spring-security-jwt/src/main/java/diospringsecurityjwt/service/UserService.java
@@ -0,0 +1,26 @@
+package diospringsecurityjwt.service;
+
+
+import diospringsecurityjwt.model.User;
+import diospringsecurityjwt.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository repository;
+    @Autowired
+    private PasswordEncoder encoder;
+
+    public UserService() {
+    }
+
+    public void createUser(User user){
+        String pass = user.getPassword();
+        //criptografando antes de salvar no banco
+        user.setPassword(encoder.encode(pass));
+        repository.save(user);
+    }
+}
