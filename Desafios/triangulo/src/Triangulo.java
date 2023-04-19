51428ea44c3ec43fb17b87dd9964400403c070b4
Maik Rodrigues <69226200+MaikRodriguess@users.noreply.github.com>
2023-04-19 11:54:27 -0300

    Desafio Triangulo completo

---
+++ Desafios/triangulo/src/Triangulo.java
@@ -0,0 +1,45 @@
+// Para ler e escrever dados em Java, aqui na DIO padronizamos da seguinte forma: 
+// - new Scanner(System.in): cria um leitor de Entradas, com métodos úteis com prefixo "next";
+// - System.out.println:.imprime um texto de Saída (Output) e pulando uma linha.  
+
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Triangulo{
+
+    public static void main(String[] args) throws IOException {
+        Scanner leitor = new Scanner(System.in);
+        double A = leitor.nextDouble();
+        double B = leitor.nextDouble();
+        double C = leitor.nextDouble();
+        double area;
+        double perimetro;
+
+
+//TODO: Implemente a condição necessária para o cálculo do triângulo:
+//Dica: Você pode utilizar o String.format() na formatação do texto
+        Triangulo trianguloClass = new Triangulo();
+
+            // Caso forme um triangulo
+        if(A+B > C && A+C > B &&  B+C > A){
+            perimetro = trianguloClass.perimetro(A, B, C);
+            System.out.printf("Perimetro = %.2f", perimetro);
+
+            // Caso não forme um triangulo
+        }else {
+            area = trianguloClass.area(A, B, C);
+            System.out.printf("Area = %.2f", area);
+        }
+    }
+
+    // AREA
+    public double area(double A, double B, double C){
+        return ((A + B) * C) / 2;
+    }
+
+    //PERIMETRO
+    public double perimetro(double A, double B, double C){
+        return A + B + C;
+    }
+}
